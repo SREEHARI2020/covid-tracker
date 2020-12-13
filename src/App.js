75b53ea08051dc76af84b06f2ad5a772f945340a
SREEHARI <sreehari2610@gmail.com>
2020-12-13 17:27:49 +0530

    components initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
-
+import{Cards,Chart,CountryPicker} from './components/Index';
 function App() {
   return (
     <div className="App">
-     
+     <Cards/>
+     <CountryPicker/>
+     <Chart/>
+   
     </div>
   );
 }
