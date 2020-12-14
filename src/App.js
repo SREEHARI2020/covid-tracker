5625fe719699544ff6804039881d30eca154c740
SREEHARI <sreehari2610@gmail.com>
2020-12-14 12:25:34 +0530

    fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import React,{useEffect} from 'react'
 import './App.css';
 import{Cards,Chart,CountryPicker} from './components/Index';
+import styles from './App.module.css';
+import { fetchData } from './api/Index';
+
 function App() {
+
+useEffect(() => {
+  const fetch=async()=>{
+    const data= await fetchData();
+    console.log(data)
+
+  }
+
+  fetch();
+ 
+}, [])
   return (
-    <div className="App">
+    <div className={styles.container}>
      <Cards/>
      <CountryPicker/>
      <Chart/>
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -2,7 +2,7 @@ import React from 'react'
  const CountryPicker = () => {
     return (
         <div>
-            
+            CountryPicker
         </div>
     )
 }
